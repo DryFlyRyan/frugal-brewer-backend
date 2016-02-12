d2d1d4ba301e60565202fdb0faf69340bc045d66
Ryan Douglas <ryan@luckydogdigital.com>
2016-02-12 13:32:53 -0700

    basic git request created

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,30 @@ require('dotenv').load();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('calling brewerydb');
+  getRequest('fermentables')
+  .then(function(data) {
+    console.log('data got');
+    console.log(data);
+    res.send(data)
+  }).catch(function(err){
+    res.JSON(err)
+  })
 });
 
-function getRequest() {
-
+function getRequest(destination, page) {
+  var currentPage = page || 1;
+  return new Promise(function(resolve, reject) {
+    unirest.get('http://api.brewerydb.com/v2/' + destination).query({
+      'key': process.env.BREWERYDB_API_KEY,
+      'p': currentPage
+    })
+    .header({'Accept': 'application/json'}, {"Access-Control-Allow-Headers": "x-requested-with, x-requested-by"})
+    .end(function (response){
+      console.log('ended');
+      resolve(response);
+}   );
+  })
 }
 
 module.exports = router;
